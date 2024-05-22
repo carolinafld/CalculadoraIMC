a735128e02aa7572da870427799faf09fd616f96
Carolina Friedrich Lobo D Avila <137728605+carolinafld@users.noreply.github.com>
2024-05-22 17:38:48 -0400

    Mostrar resultado na proxima tela

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,12 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Recuperar os componentes EditText
-        // Criar uma variável e associar o componente de UI <EditText>
-        // Recuperar o botão da tela
-        // Colocar acao no botao setOnClickListener
-        // Recuperar o texto digitado no edtPeso
 
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
@@ -28,7 +23,6 @@ class MainActivity : AppCompatActivity() {
             val alturaStr: String = edtAltura.text.toString()
 
             if (pesoStr == "" || alturaStr == "") {
-                // Mostrar mensagem para o usuário
                 Snackbar
                     .make(
                         edtPeso,
@@ -43,6 +37,17 @@ class MainActivity : AppCompatActivity() {
 
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
+
+                // Navegar para a proxima tela
+                // Criar o layuout da proxima tela
+                // Passar dados(resultado) para a proxima tela
+
+                // Intent - Classe do proprio android
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
                 println("Roque " + resultado)
             }
 
